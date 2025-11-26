b0905935421eb24347af9f8d7081ab947f1d7dcd
dafalagi <dafarizky34@gmail.com>
2025-11-26 10:13:37 +0700

    feat(auth): add soft deletes to auth_users table migration

--- modules/Auth/Database/Migrations/2024_01_01_000001_create_auth_users_table.php
+++ modules/Auth/Database/Migrations/2024_01_01_000001_create_auth_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
         
         Schema::create('auth_password_reset_tokens', function (Blueprint $table) {
