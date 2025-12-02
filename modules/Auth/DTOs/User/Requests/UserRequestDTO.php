d2fea5ac68c7257d4302063f49baf5fc31c819e8
dafalagi <dafarizky34@gmail.com>
2025-12-02 13:21:39 +0700

    feat(user-request-dto): add 'with' property to UserRequestDTO for additional data inclusion

--- modules/Auth/DTOs/User/Requests/UserRequestDTO.php
+++ modules/Auth/DTOs/User/Requests/UserRequestDTO.php
@@ -10,7 +10,8 @@ class UserRequestDTO
         public readonly ?int $page = null,
         public readonly ?string $sort_by = null,
         public readonly ?string $sort_order = null,
-        public readonly bool $with_pagination = false
+        public readonly bool $with_pagination = false,
+        public readonly ?array $with = null,
     ) {}
 
     public static function fromArray(array $data): self
@@ -22,6 +23,7 @@ class UserRequestDTO
             sort_by: $data['sort_by'] ?? null,
             sort_order: $data['sort_order'] ?? null,
             with_pagination: $data['with_pagination'] ?? false,
+            with: $data['with'] ?? null,
         );
     }
 
@@ -34,6 +36,7 @@ class UserRequestDTO
             'sort_by' => $this->sort_by,
             'sort_order' => $this->sort_order,
             'with_pagination' => $this->with_pagination,
+            'with' => $this->with,
         ];
     }
 }
