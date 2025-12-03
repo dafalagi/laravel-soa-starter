1d24ff5435a4911f574ad2f82ae44f584b2d3f66
dafalagi <dafarizky34@gmail.com>
2025-12-03 14:28:26 +0700

    fix(routes): change route prefix from 'user' to 'users' for consistency

--- modules/Auth/Routes/admin/auth/user.php
+++ modules/Auth/Routes/admin/auth/user.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\Api\Admin\UserController;
 
-Route::prefix('user')->group(function () {
+Route::prefix('users')->group(function () {
     Route::post('', [UserController::class, 'store'])->name('admin.auth.user.store');
 
     Route::get('{user_uuid}', [UserController::class, 'show'])->name('admin.auth.user.show');
